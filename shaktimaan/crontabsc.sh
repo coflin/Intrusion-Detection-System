676ac91da7d33bc34e98fef33ce492b90f091400
Sneha Irukuvajjula <snehairukuvajjula@gmail.com>
2019-03-09 09:46:50 +0530

    Update crontabsc.sh

--- shaktimaan/crontabsc.sh
+++ shaktimaan/crontabsc.sh
@@ -6,7 +6,8 @@ echo "@reboot sleep 10; /etc/Intrusion-Detection-System/shaktimaan/Reports.sh >>
 echo "@reboot sleep 10; /etc/Intrusion-Detection-System/shaktimaan/ShutDownreport.sh >> /etc/Intrusion-Detection-System/shaktimaan/logs/ShutdownReport" >> mycron 
 echo "@reboot sleep 10; /etc/Intrusion-Detection-System/shaktimaan/RebootReport.sh >> /etc/Intrusion-Detection-System/shaktimaan/logs/RebootReport" >> mycron 
 echo "*/2 * * * * /etc/Intrusion-Detection-System/shaktimaan/dos.sh" >> mycron 
-echo "@reboot sleep 10; /etc/Intrusion-Detection-System/shaktimaan/encrypt" >> mycron
+echo "@reboot /etc/Intrusion-Detection-System/shaktimaan/encrypt" >> mycron
+echo "@reboot /etc/Intrusion-Detection-System/shaktimaan/fksencrypt" >> mycron
 #install new cron file
 crontab mycron
 rm mycron
